47b98ff13eb9eabe7a84bbef0b1a8d60bb2cea0b
Patryk Chodur <patrykchodur@icloud.com>
2021-01-05 22:49:09 +0100

    script expanded

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -8,5 +8,21 @@
 
 
 # new with ssh server
-docker build -t gemroc:1.0 -f ./Dockerfile .
-docker run -d -p 22:22 gemroc:1.0
+docker build \
+	--build-arg USERNAME=gemroc \
+	--build-arg PASSWORD=gemroc1 \
+	--build-arg WIRESHARK_VERSION=3.4.2 \
+	-t gemroc:1.0 \
+	-f ./Dockerfile \
+	.
+
+build_status=$?
+
+if [ $build_status -ne 0 ]; then
+	echo "Error: build failed"
+	exit 1
+else
+	echo "Docker built successfully"
+	docker run -d -p 22:22 gemroc:1.0
+	exit $?
+fi
